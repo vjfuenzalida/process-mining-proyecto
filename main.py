d63369c36d001a13de796299946516748c49b5f4
Vicente Fuenzalida <vjfuenzalida@uc.cl>
2018-05-01 04:18:33 -0300

    initial routine to generate logs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
+from collections import OrderedDict
 from reader import open_excel
-from models import CourseInstance, CourseInstanceRaw, Student, StudentRaw
+from models import Course, CourseRaw, Student, StudentRaw, Requirement, timestamp_after
+
 
 def dump_data():
     years = [2013, 2014, 2015, 2016]
@@ -27,14 +29,14 @@ def dump_data():
             StudentRaw(**student_datum)
 
         for course_datum in courses_data[year]:
-            CourseInstanceRaw(**course_datum)
+            CourseRaw(**course_datum)
 
     with open('processed_data/student_objects.json', 'w', encoding='utf8') as student_objects_file:
         data = [s.__dict__ for s in StudentRaw.all]
         json.dump(data, student_objects_file)
 
     with open('processed_data/course_objects.json', 'w', encoding='utf8') as course_objects_file:
-        data = [c.__dict__ for c in CourseInstanceRaw.all]
+        data = [c.__dict__ for c in CourseRaw.all]
         json.dump(data, course_objects_file)
 
     with open('processed_data/student_dict.json', 'w', encoding='utf8') as student_dict_file:
@@ -42,32 +44,130 @@ def dump_data():
         json.dump(data, student_dict_file)
 
     with open('processed_data/course_dict.json', 'w', encoding='utf8') as course_dict_file:
-        data = CourseInstanceRaw.prop_names
+        data = CourseRaw.prop_names
         json.dump(data, course_dict_file)
 
     with open('processed_data/student_field_possibilities.json', 'w', encoding='utf8') as student_field_possibilities_file:
         data = StudentRaw.prop_names
-        field_hash = {v: list(set([getattr(s, v) for s in StudentRaw.all])) for v in data.values()}
+        field_hash = {
+            v: list(set([getattr(s, v) for s in StudentRaw.all])) for v in data.values()}
         json.dump(field_hash, student_field_possibilities_file)
 
     with open('processed_data/course_field_possibilities.json', 'w', encoding='utf8') as course_field_possibilities_file:
-        data = CourseInstanceRaw.prop_names
-        field_hash = {v: list(set([getattr(s, v) for s in CourseInstanceRaw.all])) for v in data.values()}
+        data = CourseRaw.prop_names
+        field_hash = {
+            v: list(set([getattr(s, v) for s in CourseRaw.all])) for v in data.values()}
         json.dump(field_hash, course_field_possibilities_file)
+
 
 def load_objects():
     with open('processed_data/student_objects.json', 'r', encoding='utf-8') as student_objects_file:
         data = json.load(student_objects_file)
-        objects = [Student(datum) for datum in data]
-        Student.all = objects
+        [Student(datum) for datum in data]
     with open('processed_data/course_objects.json', 'r', encoding='utf-8') as course_objects_file:
         data = json.load(course_objects_file)
-        objects = [CourseInstance(datum) for datum in data]
-        CourseInstance.all = objects
+        [Course(datum) for datum in data]
+
+
+def load_requirements():
+    with open('data/requirements.json', 'r', encoding='utf-8') as requirements_file:
+        data = json.load(requirements_file)
+        [Requirement(**datum) for datum in data]
 
 
 if __name__ == '__main__':
-    # dump_data()
+        # dump_data()
+    load_requirements()
     load_objects()
-    print(Student.filter_by('admission_way', 'ORDINARIA PAA'))
-    # print(CourseInstance.filter_by('initials', 'ING1004'))
+
+    potential_requirements = []
+    for course in Requirement.all.values():
+        potential_requirements += course.valid_initials
+
+    Student.all = Student.filter_by('admission_way', 'ORDINARIA PAA')
+    Student.all = Student.filter_by('admission_year', '2013')
+    Student.all = Student.filter_by('curriculum', 'C2013')
+    Student.all = Student.filter_by('admission_type', '-')
+
+    valid_students = list(Student.all.keys())
+    Course.all = Course.from_students(valid_students)
+    Course.all = Course.with_initials(potential_requirements)
+
+    for course in Course.all.values():
+        Student.all[course.student_id].courses[course.initials].append(course)
+        Student.all[course.student_id].semesters[course.relative_semester()].append(
+            course)
+
+    semester_timestamps = {
+        1: {
+            "start_date": "03/05",
+            "end_date": "07/04",
+        },
+        2: {
+            "start_date": "08/04",
+            "end_date": "12/15",
+        },
+        3: {
+            "start_date": "01/04",
+            "end_date": "02/22",
+        }
+    }
+
+    # 1º, 2º and TAV for 5 years (from 2013 to the end of 2017)
+    logs = ["Case ID,Start Timestamp,End Timestamp,Activity\n"]
+
+    progress = Student.all['3612013'].progress_per_semester()
+    for i in progress.values():
+        print(i)
+
+    for student_id, student in Student.all.items():
+        progresses = student.progress_per_semester()
+        initial_log = "{},{},{},{}\n".format(
+            student.student_number, "2013/03/01", "2013/03/02", "Start university career")
+        print(initial_log)
+        logs.append(initial_log)
+        last_progress = None
+        for semester_number, data in progresses.items():
+            activity = data['activity']
+            if activity == None:
+                continue
+            last_progress = data
+            activity_log = "{},{},{},{}\n".format(
+                student.student_number, data['start_timestamp'], data['end_timestamp'], activity)
+
+            tav_semester = False    
+            semester_activity = "End of semester {}".format(
+                semester_number - int(semester_number / 3))
+            if semester_number % 3 == 0:
+                semester_activity = "End of TAV semester {}".format(
+                    int(semester_number / 3))
+                tav_semester = True
+            semester_start = data['semester_start']
+            semester_end = data['semester_end']
+            semester_log = "{},{},{},{}\n".format(
+                student.student_number, semester_start, semester_end, semester_activity)
+            if tav_semester and data['taken'] == 0:
+                continue
+            print(semester_log)
+            logs.append(semester_log)
+            print(activity_log)
+            logs.append(activity_log)
+        if (not student.requirements_ready) and last_progress:
+            start_date = timestamp_after(last_progress['start_timestamp'], 2)
+            end_date = timestamp_after(last_progress['end_timestamp'], 2)
+            accumulated = last_progress['accumulated']
+            percentage = last_progress['percentage']
+            unqualified_activity = "Not qualified to take the exam. {}/16 ({}%) completed".format(
+                accumulated, percentage)
+            incomplete_requirements_log = "{},{},{},{}\n".format(
+                student.student_number, start_date, end_date, unqualified_activity)
+            logs.append(incomplete_requirements_log)
+            print(incomplete_requirements_log)
+        final_log = "{},{},{},{}\n".format(
+            student.student_number, "2018/05/01", "2018/05/02", "Today")
+        print(final_log)
+        logs.append(final_log)
+
+    with open('processed_data/logs.csv', 'w', encoding='utf-8') as log_file:
+        for log in logs:
+            log_file.write(log)
--- models.py
+++ models.py
@@ -1,25 +1,190 @@
+from collections import defaultdict
+
+
+def get_activity(taken, approved, accumulated):
+    # LISTO
+    percentage = accumulated * 100 / 16
+    if accumulated == 16:
+        if taken == 0:
+            return None
+        else:
+            return "Qualified to take the exam. 16/16 (100%) completed"
+    # NO TOMÓ CURSOS
+    elif taken == 0:
+        return "No requirements taken this semester"
+    # TOMÓ CURSOS Y PROGRESÓ
+    elif taken > 0 and approved > 0:
+        return "Approved new requirements. {}/16 ({}%) completed".format(accumulated, percentage)
+    # TOMÓ CURSOS Y NO PROGRESÓ
+    elif taken > 0 and approved == 0:
+        return "No approved requirements. {}/16 ({}%) completed".format(accumulated, percentage)
+
+
+def timestamp_before(date, days):
+    parsed = date.split("/")
+    year = int(parsed[0])
+    month = int(parsed[1])
+    day = int(parsed[2])
+    return "{:04d}/{:02d}/{:02d}".format(year, month, day - days)
+
+
+def timestamp_after(date, days):
+    parsed = date.split("/")
+    year = int(parsed[0])
+    month = int(parsed[1])
+    day = int(parsed[2])
+    return "{:04d}/{:02d}/{:02d}".format(year, month, day + days)
+
+
 class Student:
 
-    all = []
+    all = {}
 
     def __init__(self, data):
         self.__dict__ = data
+        self.student_id = "{}{}".format(
+            data['student_number'], data['admission_year'])
+        Student.all.update({self.student_id: self})
+        self.courses = defaultdict(list)
+        self.semesters = defaultdict(list)
+        self.semester_count = 3 * (2018 - int(data['admission_year']))
+        self.requirements_ready = False
+        self.requirements_ready_at = ""
 
     @classmethod
     def filter_by(cls, field, value):
-        return list(filter(lambda s: getattr(s, field) == value, cls.all))
+        return {k: v for k, v in cls.all.items() if getattr(v, field) == value}
+
+    def progress_per_semester(self):
+        progresses = {}
+        accumulated = 0
+        for semester_number in range(1, self.semester_count + 1):
+            semester = int(semester_number)
+            start_timestamp, end_timestamp = Course.timestamp_from_relative(
+                semester, self.admission_year)
+            if semester in self.semesters.keys():
+                courses = self.semesters[semester]
+                taken = len(courses)
+                approved = len([c for c in courses if float(
+                    c.final_grade) >= 4.0 or c.alpha_final_grade == "A"])
+            else:
+                courses = []
+                taken = 0
+                approved = 0
+            failed = taken - approved
+            accumulated += approved
+            percentage = accumulated * 100 / 16
+            if accumulated == 16:
+              self.requirements_ready = True
+              self.requirements_ready_at = semester_number
+            progresses.update(
+                {semester: {
+                    "semester": semester,
+                    "percentage": percentage,
+                    "approved": approved,
+                    "failed": failed,
+                    "taken": taken,
+                    "accumulated": accumulated,
+                    "total": 16,
+                    "semester_start": start_timestamp,
+                    "semester_end": end_timestamp,
+                    "start_timestamp": timestamp_after(end_timestamp, 3),
+                    "end_timestamp": timestamp_after(end_timestamp, 4),
+                    "activity": get_activity(taken, approved, accumulated)
+                }})
+        return progresses
 
 
-class CourseInstance:
+class Course:
 
-    all = []
+    all = {}
+
+    # format month/day
+    timestamps = {
+        1: {
+            "start_date": "03/05",
+            "end_date": "07/04",
+        },
+        2: {
+            "start_date": "08/04",
+            "end_date": "12/15",
+        },
+        3: {
+            "start_date": "01/04",
+            "end_date": "02/22",
+        }
+    }
 
     def __init__(self, data):
         self.__dict__ = data
+        self.course_id = "{}{}{}".format(
+            data['rut'], data['initials'], data['semester'])
+        self.case_id = ['student_number']
+        self.student_id = "{}{}".format(
+            data['student_number'], data['admission_year'])
+        self.start_timestamp, self.end_timestamp = Course.timestamp_from_absolute(
+            data['semester'], data['year'])
+        self.grade = data['final_grade'] if data['final_grade'] != "nan" else data['alpha_final_grade']
+
+        Course.all.update({self.course_id: self})
 
     @classmethod
     def filter_by(cls, field, value):
-        return list(filter(lambda s: getattr(s, field) == value, cls.all))
+        return {k: v for k, v in cls.all.items() if getattr(v, field) == value}
+
+    @classmethod
+    def timestamp_from_relative(cls, relative_semester, admission_year):
+        relative_semester = int(relative_semester)
+        semester = 3 if relative_semester % 3 == 0 else relative_semester % 3
+        start = cls.timestamps[semester]['start_date']
+        end = cls.timestamps[semester]['end_date']
+        year = int(admission_year) + int(relative_semester / 3)
+        return ["{}/{}".format(year, start), "{}/{}".format(year, end)]
+
+    @classmethod
+    def timestamp_from_absolute(cls, absolute_semester, year):
+        semester = int(absolute_semester)
+        start = cls.timestamps[semester]['start_date']
+        end = cls.timestamps[semester]['end_date']
+        year = int(year) + int(semester / 3)
+        return ["{}/{}".format(year, start), "{}/{}".format(year, end)]
+
+    @classmethod
+    def with_initials(cls, initials):
+        return {k: v for k, v in cls.all.items() if v.initials in initials}
+
+    @classmethod
+    def from_students(cls, students):
+        return {k: v for k, v in cls.all.items() if v.student_id in students}
+
+    def __repr__(self):
+        return "{}-{} {}-{}: {} ({})".format(self.year, self.semester, self.initials, self.section, self.course_name, self.grade)
+
+    def relative_semester(self):
+        relative_year = int(self.year) - int(self.admission_year)
+        semester = relative_year * 3 + int(self.semester)
+        return int(semester)
+
+
+class Requirement:
+
+    all = {}
+
+    def __init__(self, **data):
+        self.initials = data['initials']
+        self.valid_initials = data['valid_initials'] + [self.initials]
+        self.name = data['name']
+        self.semester = data['semester']
+
+        if self.valid_initials:
+            for initial in self.valid_initials:
+                Requirement.all.update({initial: self})
+
+
+# class ProgressActivity:
+
+    # JUST TO DUMP DATA!
+
 
 class StudentRaw:
     all = []
@@ -67,68 +232,69 @@ class StudentRaw:
     }
 
     def __init__(self, **kwargs):
-        self.rut = kwargs.get('RUT', None)
-        self.student_number = kwargs.get('N°ALUMNO', None)
-        self.curriculum = kwargs.get('CURRICULUM', None)
-        self.program_code = kwargs.get('CODIGO PROGRAMA', None)
-        self.program = kwargs.get('PROGRAMA', None)
-        self.specialty_preference_code = kwargs.get(
-            'CODIGO PREFERENCIA ESPECIALIDAD', None)
-        self.specialty_preference = kwargs.get(
-            'PREFERENCIA ESPECIALIDAD', None)
-        self.major_preference_code = kwargs.get(
-            'CODIGO PREFERENCIA MAJOR', None)
-        self.major_preference = kwargs.get('PREFERENCIA MAJOR', None)
-        self.selected_major_code = kwargs.get(
-            'CODIGO MAJOR SELECCIONADO', None)
-        self.selected_major = kwargs.get('MAJOR SELECCIONADO', None)
-        self.selected_minor_code = kwargs.get(
-            'CODIGO MINOR SELECCIONADO', None)
-        self.selected_minor = kwargs.get('MINOR SELECCIONADO', None)
-        self.track = kwargs.get('TRACK/ÁREA', None)
-        self.customized_studies_plan = kwargs.get(
-            'PLAN DE ESTUDIOS PERSONALIZADO', None)
-        self.state = kwargs.get('ESTADO', None)
-        self.admission_year = kwargs.get('AÑO INGRESO', None)
-        self.admission_way = kwargs.get('VÍA DE INGRESO', None)
-        self.special_admission_type = kwargs.get('TIPO INGRESO ESPECIAL', None)
-        self.ppa = kwargs.get('PROM. PPA', None)
-        self.total_approved_credits = kwargs.get(
-            'TOTAL CRÉDITOS APROBADOS', None)
-        self.total_validated_credits = kwargs.get(
-            'TOTAL CRÉDITOS CONVALIDADOS', None)
-        self.total_approved_and_validated_credits = kwargs.get(
-            'TOTAL CRÉDITOS APROBADOS+CONVALIDADOS', None)
-        self.total_failed_credits = kwargs.get(
-            'TOTAL CRÉDITOS REPROBADOS', None)
-        self.total_enrolled_credits = kwargs.get(
-            'TOTAL CRÉDITOS INSCRITOS', None)
-        self.elimination_causals_number = kwargs.get(
-            'N° CAUSALES ELIMINACIÓN (POR SEMESTRE)', None)
-        self.valid_studies = kwargs.get('ESTUDIOS VIGENTES(PARALELO)', None)
-        self.invalid_studies = kwargs.get(
-            'ESTUDIOS NO VIGENTES(FINALIZADOS POR ALGÚN MOTIVO)', None)
-        self.licensed = kwargs.get('LICENCIADO', None)
-        self.licensed_at = kwargs.get('FECHA LIC.', None)
-        self.graduated = kwargs.get('EGRESADO', None)
-        self.graduated_at = kwargs.get('FECHA EGR.', None)
-        self.tituled = kwargs.get('TITULADO', None)
-        self.school_region = kwargs.get('COLEGIO REGIÓN', None)
-        self.school_type = kwargs.get('COLEGIO TIPO', None)
-        self.school_of_graduation = kwargs.get('COLEGIO EGRESO', None)
-        self.dsrd_current_state = kwargs.get('ESTADO DSRD ACTUAL', None)
-        self.admission_type = kwargs.get('TIPO INGRESO', None)
-        self.admission_ranking = kwargs.get('PUESTO', None)
+        self.rut = str(kwargs.get('RUT', ''))
+        self.student_number = str(kwargs.get('N°ALUMNO', ''))
+        self.curriculum = str(kwargs.get('CURRICULUM', ''))
+        self.program_code = str(kwargs.get('CODIGO PROGRAMA', ''))
+        self.program = str(kwargs.get('PROGRAMA', ''))
+        self.specialty_preference_code = str(kwargs.get(
+            'CODIGO PREFERENCIA ESPECIALIDAD', ''))
+        self.specialty_preference = str(kwargs.get(
+            'PREFERENCIA ESPECIALIDAD', ''))
+        self.major_preference_code = str(kwargs.get(
+            'CODIGO PREFERENCIA MAJOR', ''))
+        self.major_preference = str(kwargs.get('PREFERENCIA MAJOR', ''))
+        self.selected_major_code = str(kwargs.get(
+            'CODIGO MAJOR SELECCIONADO', ''))
+        self.selected_major = str(kwargs.get('MAJOR SELECCIONADO', ''))
+        self.selected_minor_code = str(kwargs.get(
+            'CODIGO MINOR SELECCIONADO', ''))
+        self.selected_minor = str(kwargs.get('MINOR SELECCIONADO', ''))
+        self.track = str(kwargs.get('TRACK/ÁREA', ''))
+        self.customized_studies_plan = str(kwargs.get(
+            'PLAN DE ESTUDIOS PERSONALIZADO', ''))
+        self.state = str(kwargs.get('ESTADO', ''))
+        self.admission_year = str(kwargs.get('AÑO INGRESO', ''))
+        self.admission_way = str(kwargs.get('VÍA DE INGRESO', ''))
+        self.special_admission_type = str(
+            kwargs.get('TIPO INGRESO ESPECIAL', ''))
+        self.ppa = str(kwargs.get('PROM. PPA', ''))
+        self.total_approved_credits = str(kwargs.get(
+            'TOTAL CRÉDITOS APROBADOS', ''))
+        self.total_validated_credits = str(kwargs.get(
+            'TOTAL CRÉDITOS CONVALIDADOS', ''))
+        self.total_approved_and_validated_credits = str(kwargs.get(
+            'TOTAL CRÉDITOS APROBADOS+CONVALIDADOS', ''))
+        self.total_failed_credits = str(kwargs.get(
+            'TOTAL CRÉDITOS REPROBADOS', ''))
+        self.total_enrolled_credits = str(kwargs.get(
+            'TOTAL CRÉDITOS INSCRITOS', ''))
+        self.elimination_causals_number = str(kwargs.get(
+            'N° CAUSALES ELIMINACIÓN (POR SEMESTRE)', ''))
+        self.valid_studies = str(kwargs.get('ESTUDIOS VIGENTES(PARALELO)', ''))
+        self.invalid_studies = str(kwargs.get(
+            'ESTUDIOS NO VIGENTES(FINALIZADOS POR ALGÚN MOTIVO)', ''))
+        self.licensed = str(kwargs.get('LICENCIADO', ''))
+        self.licensed_at = str(kwargs.get('FECHA LIC.', ''))
+        self.graduated = str(kwargs.get('EGRESADO', ''))
+        self.graduated_at = str(kwargs.get('FECHA EGR.', ''))
+        self.tituled = str(kwargs.get('TITULADO', ''))
+        self.school_region = str(kwargs.get('COLEGIO REGIÓN', ''))
+        self.school_type = str(kwargs.get('COLEGIO TIPO', ''))
+        self.school_of_graduation = str(kwargs.get('COLEGIO EGRESO', ''))
+        self.dsrd_current_state = str(kwargs.get('ESTADO DSRD ACTUAL', ''))
+        self.admission_type = str(kwargs.get('TIPO INGRESO', ''))
+        self.admission_ranking = str(kwargs.get('PUESTO', ''))
         StudentRaw.all.append(self)
 
 
-class CourseInstanceRaw:
+class CourseRaw:
     all = []
 
     prop_names = {
         'RUT': 'rut',
         'N°ALUMNO': 'student_number',
-        'SEXO': 'sexo',
+        'SEXO': 'sex',
         'AÑO ADMISIÓN': 'admission_year',
         'PROGRAMA CODIGO': 'program_code',
         'PROGRAMA': 'program',
@@ -155,39 +321,39 @@ class CourseInstanceRaw:
     }
 
     def __init__(self, **kwargs):
-        self.rut = kwargs.get('RUT', None)
-        self.student_number = kwargs.get('N°ALUMNO', None)
-        self.sexo = kwargs.get('SEXO', None)
-        self.admission_year = kwargs.get('AÑO ADMISIÓN', None)
-        self.program_code = kwargs.get('PROGRAMA CODIGO', None)
-        self.program = kwargs.get('PROGRAMA', None)
-        self.selected_major_code = kwargs.get(
-            'MAJOR CODIGO SELECCIONADO', None)
-        self.selected_major = kwargs.get('MAJOR SELECCIONADO', None)
-        self.track = kwargs.get('MAJOR TRACK/ÁREA ', None)
-        self.selected_minor_code = kwargs.get(
-            'MINOR CODIGO SELECCIONADO', None)
-        self.selected_minor = kwargs.get('MINOR SELECCIONADO', None)
-        self.student_credits = kwargs.get('CREDITOS ALUMNO', None)
-        self.curriculum_course = kwargs.get('CURSO CURRICULUM', None)
-        self.program_course = kwargs.get('CURSO PROGRAMA', None)
-        self.year = kwargs.get('AÑO', None)
-        self.semester = kwargs.get('SEMESTRE', None)
-        self.initials = kwargs.get('SIGLA', None)
-        self.section = kwargs.get('SECCIÓN', None)
-        self.course_name = kwargs.get('NOMBRE CURSO', None)
-        self.course_credits = kwargs.get('CREDITOS CURSO', None)
-        self.final_grade = kwargs.get('NOTA FINAL', None)
-        self.alpha_final_grade = kwargs.get('NOTA FINAL ALFA', None)
-        self.global_ppa = kwargs.get('PPA Global', None)
-        self.dara_state = kwargs.get('Estado en DARA', None)
-        self.admission_way = kwargs.get('VIA INGRESO', None)
-        self.admission_case_way = kwargs.get('VIA CASO INGRESO', None)
-        CourseInstanceRaw.all.append(self)
+        self.rut = str(kwargs.get('RUT', ''))
+        self.student_number = str(kwargs.get('N°ALUMNO', ''))
+        self.sex = str(kwargs.get('SEXO', ''))
+        self.admission_year = str(kwargs.get('AÑO ADMISIÓN', ''))
+        self.program_code = str(kwargs.get('PROGRAMA CODIGO', ''))
+        self.program = str(kwargs.get('PROGRAMA', ''))
+        self.selected_major_code = str(kwargs.get(
+            'MAJOR CODIGO SELECCIONADO', ''))
+        self.selected_major = str(kwargs.get('MAJOR SELECCIONADO', ''))
+        self.track = str(kwargs.get('MAJOR TRACK/ÁREA ', ''))
+        self.selected_minor_code = str(kwargs.get(
+            'MINOR CODIGO SELECCIONADO', ''))
+        self.selected_minor = str(kwargs.get('MINOR SELECCIONADO', ''))
+        self.student_credits = str(kwargs.get('CREDITOS ALUMNO', ''))
+        self.curriculum_course = str(kwargs.get('CURSO CURRICULUM', ''))
+        self.program_course = str(kwargs.get('CURSO PROGRAMA', ''))
+        self.year = str(kwargs.get('AÑO', ''))
+        self.semester = str(kwargs.get('SEMESTRE', ''))
+        self.initials = str(kwargs.get('SIGLA', ''))
+        self.section = str(kwargs.get('SECCIÓN', ''))
+        self.course_name = str(kwargs.get('NOMBRE CURSO', ''))
+        self.course_credits = str(kwargs.get('CREDITOS CURSO', ''))
+        self.final_grade = str(kwargs.get('NOTA FINAL', ''))
+        self.alpha_final_grade = str(kwargs.get('NOTA FINAL ALFA', ''))
+        self.global_ppa = str(kwargs.get('PPA Global', ''))
+        self.dara_state = str(kwargs.get('Estado en DARA', ''))
+        self.admission_way = str(kwargs.get('VIA INGRESO', ''))
+        self.admission_case_way = str(kwargs.get('VIA CASO INGRESO', ''))
+        CourseRaw.all.append(self)
 
 
 if __name__ == '__main__':
     print("This module doesn't run code. Try running the 'main.py' file!")
 
     # for k, v in course_prop_names.items():
-    #   print("self.{} = kwargs.get('{}', None)".format(v, k))
+    #   print("self.{} = str(kwargs.get('{}', ''))".format(v, k))
